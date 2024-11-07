40961a3526f0dec8028567ed6ec4bd5c1753e9cc
DogGang <69455222+doggang@users.noreply.github.com>
2024-11-07 22:25:40 +0900

    [Bronze V] Title: 사칙연산, Time: 104 ms, Memory: 9608 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,8 @@
+const input = require("fs").readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim().split(" ").map(Number);
+const a = input[0];
+const b = input[1];
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(parseInt(a/b));
+console.log(a%b);

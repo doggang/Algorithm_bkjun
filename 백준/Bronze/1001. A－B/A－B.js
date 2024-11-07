1c0d1a465a57d2e41e16f40e3e52373231067339
DogGang <69455222+doggang@users.noreply.github.com>
2024-11-07 22:16:43 +0900

    [Bronze V] Title: A-B, Time: 100 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.js
@@ -0,0 +1,5 @@
+const input = require("fs").readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim().split(" ").map(Number);
+
+const a = input[0];
+const b = input[1];
+console.log(a-b);

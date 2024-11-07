07d4cf8166dd57d8f7687c57c26d1686c39b0010
DogGang <69455222+doggang@users.noreply.github.com>
2024-11-07 22:19:38 +0900

    [Bronze V] Title: A×B, Time: 100 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.js
@@ -0,0 +1,4 @@
+const input = require("fs").readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim().split(" ").map(Number);
+const a = input[0];
+const b= input[1];
+console.log(a*b);

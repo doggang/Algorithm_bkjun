2b6c925f6bec80c2ec13efcd8faa2dcfe37e8557
DogGang <69455222+doggang@users.noreply.github.com>
2025-04-17 21:28:08 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 92 ms, Memory: 9352 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.js
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.js
@@ -1,7 +1,6 @@
-const fs = require('fs');
-let input = fs.readFileSync(0, 'utf-8').toString().trim();
-const ans = ['c=','c-','dz=','d-','lj','nj','s=','z='];
-for(let i of ans){
+let input = require("fs").readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim();
+const arr = ["c=","c-","dz=","d-","lj","nj","s=","z="];
+for(let i of arr){
     input = input.replaceAll(i,'a');
 }
 console.log(input.length);

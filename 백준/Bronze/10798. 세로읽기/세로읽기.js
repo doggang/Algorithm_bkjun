773b0afafa885ef2bff707b7d8af327e25059968
DogGang <69455222+doggang@users.noreply.github.com>
2025-04-19 21:09:49 +0900

    [Bronze I] Title: 세로읽기, Time: 104 ms, Memory: 9380 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.js
+++ 백준/Bronze/10798. 세로읽기/세로읽기.js
@@ -1,17 +1,15 @@
-const fs = require('fs');
-const input = fs.readFileSync(0, 'utf-8').toString().trim().split("\n").map((el)=>el.split(''));
+const input = require("fs").readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim().split("\n");
 let max = 0;
-let ans = '';
-
 for(let i=0; i<input.length; i++){
-    if(input[i].length > max){
-        max = input[i].length;
-    }
+  if(input[i].length > max){
+    max = input[i].length;
+  }
 }
 for(let i=0; i<max; i++){
-    for(let j=0; j<input.length; j++){
-        if(input[j][i] !== undefined){
-            process.stdout.write(input[j][i]);
-        }
+  for(let j=0; j<input.length; j++){
+    if(input[j][i]==undefined){
+      continue;
     }
+    process.stdout.write(input[j][i]);
+  }
 }

236a212fc306422e4c27cd84f5fb81f7bbc99250
DogGang <69455222+doggang@users.noreply.github.com>
2025-04-19 16:50:20 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 96 ms, Memory: 9480 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.js
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.js
@@ -1,19 +1,15 @@
-const fs = require('fs');
-const input = fs.readFileSync(0, 'utf-8').toString().trim().split('\n');
-const arr = [];
-for(let i=1; i<input.length; i++){
-    arr.push(input[i]);
-}
+const input = require("fs").readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim().split("\n");
+input.splice(0,1);
 let temp = '';
-let answer = arr.length;
-arr.forEach((item)=>{
+let length = input.length;
+input.forEach(item => {
     temp = item[0];
     for(let i=1; i<item.length; i++){
-        if(temp.includes(item[i]) && item[i]!==item[i-1]){
-            answer--;
+        if(temp.includes(item[i]) && item[i] !== item[i-1]){
+            length--;
             break;
         }
         temp+=item[i];
     }
 });
-console.log(answer);
+console.log(length);

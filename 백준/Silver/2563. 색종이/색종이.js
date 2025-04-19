6d86055fbeef0430190bc82155da75af5550b560
DogGang <69455222+doggang@users.noreply.github.com>
2025-04-19 22:20:51 +0900

    [Silver V] Title: 색종이, Time: 92 ms, Memory: 9908 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.js
+++ 백준/Silver/2563. 색종이/색종이.js
@@ -1,22 +1,28 @@
-const fs = require('fs');
-const input = fs.readFileSync(0, 'utf-8').toString().trim().split("\n").map((el)=>el.split(' ').map(Number));
-const row = 100;
-const col = 100;
-const count = input[0];
-let ex = 0;
-input.splice(0,1);
-let arr = Array.from(Array(row), ()=> Array(col).fill(0));
+const input = require("fs").readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim().split("\n").map((el)=>el.split(" ").map(Number));
+const num = input.splice(0,1)[0][0];
+let arr = Array.from({ length: 100 }, () => Array(100).fill(0));
 
-for(let i=0;  i<count ;i++){
-    let x = input[i][0];
-    let y = input[i][1];
+for (let i = 0; i < 100; i++) {
+  arr[i] = []; // 행 초기화
+  for (let j = 0; j < 100; j++) {
+    arr[i][j] = 0; // 기본값 0
+  }
+}
+
+for(let i=0; i<num; i++){
+  let x = input[i][0];
+  let y = input[i][1];
     for(let j=0; j<10; j++){
-        for(let k=0; k<10; k++){
-            if(arr[x+j][y+k] == 0){
-                arr[x+j][y+k] = 1;
-                ex++;
-            }
-        }
+      for(let k=0; k<10; k++){
+        arr[x+j][y+k] = 1;
+      }
     }
 }
-console.log(ex);
+
+let result = 0;
+for (let i = 0; i < 100; i++) {
+  for (let j = 0; j < 100; j++) {
+    if (arr[i][j] === 1) result++;
+  }
+}
+console.log(result);

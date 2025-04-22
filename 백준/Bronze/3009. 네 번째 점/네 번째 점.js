2a137a5bee0bbfd5c20b1c5bfa0ed24f2ab1c37a
DogGang <69455222+doggang@users.noreply.github.com>
2025-04-22 23:06:12 +0900

    [Bronze III] Title: 네 번째 점, Time: 96 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.js
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.js
@@ -1,20 +1,50 @@
-const fs = require('fs');
-const input = fs.readFileSync(0, 'utf-8').trim().toString().split('\n').map((el) => el.split(' ').map(Number));
-const x = [input[0][0], input[1][0], input[2][0]];
-const y = [input[0][1], input[1][1], input[2][1]];
-let ans = [];
-if (x[0] === x[1]) {
-    ans.push(x[2]);
-} else if (x[0] === x[2]) {
-    ans.push(x[1]);
-} else {
-    ans.push(x[0]);
+const input = require("fs").readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim().split("\n").map((el)=>el.split(" ").map(Number));
+let Xmax = 0; 
+let Xmin = 10000;
+let Ymax = 0;
+let Ymin = 10000;
+input.forEach(item => {
+  if(item[0] > Xmax){
+    Xmax = item[0];
+  }
+  if(item[0] < Xmin){
+    Xmin = item[0];
+  }
+  if(item[1] > Ymax){
+    Ymax = item[1];
+  }
+  if(item[1] < Ymin){
+    Ymin = item[1];
+  }
+});
+let ansX = 0 ;
+let ansY = 0;
+let max = 0;
+let min = 0;
+for(let j=0; j<2; j++){
+  for(let i=0; i<3; i++){
+    if(input[i][j] == Xmax && j==0){
+      max++;
+    }  else if(input[i][j] !== Xmax && j==0){
+      min++;
+    }
+    if(input[i][j] == Ymax && j==1){
+      max++;
+    }  else if(input[i][j] !== Ymax && j==1){
+      min++;
+    }
+  }
+  if(max>min && j==0){
+    ansX = Xmin;
+  }else if(max<min && j==0){
+    ansX=Xmax;
+  }
+  if(max>min && j==1){
+    ansY = Ymin;
+  }else if(max<min && j==1){
+    ansY=Ymax;
+  }
+  max=0;
+  min=0;
 }
-if (y[0] === y[1]) {
-    ans.push(y[2]);
-} else if (y[0] === y[2]) {
-    ans.push(y[1]);
-} else {
-    ans.push(y[0]);
-}
-console.log(ans[0], ans[1]);
+console.log(ansX, ansY);

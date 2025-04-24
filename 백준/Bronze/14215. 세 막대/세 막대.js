8211f2169ee9c86df0b292918d992d841d6acb39
DogGang <69455222+doggang@users.noreply.github.com>
2025-04-24 21:03:13 +0900

    [Bronze III] Title: 세 막대, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/14215. 세 막대/세 막대.js
+++ 백준/Bronze/14215. 세 막대/세 막대.js
@@ -1,15 +1,8 @@
-const fs = require('fs');
-const input = fs.readFileSync(0, 'utf-8').trim().toString().split(" ").map(Number);
-let max = Number(input.splice(input.indexOf(Math.max(...input)),1));
-let min = Number(input.splice(input.indexOf(Math.min(...input)),1));
-let mid = input[0];
-if(max==min && min==mid){
-    console.log(max+min+mid);
-}else if(max>=min+mid){
-    while(max>=min+mid){
-        max--;
-    }
-    console.log(max+min+mid);
+const input = require("fs").readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim().split(" ").map(Number);
+let long = input.splice(input.indexOf(Math.max(...input)),1)[0];
+if(long >= input[0]+input[1]){
+    long=input[0]+input[1]-1;
+    console.log(long+input[0]+input[1]);
 }else{
-    console.log(max+min+mid);
+    console.log(long+input[0]+input[1])
 }

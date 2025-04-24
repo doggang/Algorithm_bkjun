d005cdf990b0c101f417107ef59f7985650b15f4
DogGang <69455222+doggang@users.noreply.github.com>
2025-04-24 20:16:36 +0900

    [Bronze III] Title: 대지, Time: 244 ms, Memory: 25824 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.js
+++ 백준/Bronze/9063. 대지/대지.js
@@ -1,10 +1,38 @@
-const fs = require('fs');
-const input = fs.readFileSync(0, 'utf-8').trim().toString().split("\n").map((el)=>el.split(' ').map(Number));
-const count = input.splice(0,1);
-let x =[];
-let y =[];
-for(let i=0; i<count; i++){
-    x.push(input[i][0]);
-    y.push(input[i][1]);
+const input = require("fs").readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim().split("\n").map((el)=>el.split(" ").map(Number));
+input.splice(0,1);
+let xMax=-10001;
+let xMin=100001;
+let yMax=-10001;
+let yMin=100001;
+
+input.forEach(item => {
+    for(let i=0; i<2; i++){
+        if(item[i] > xMax && i==0){
+            xMax = item[i];
+        }
+        if(item[i] < xMin && i==0){
+            xMin = item[i];
+        }
+
+        if(item[i] > yMax && i==1){
+            yMax = item[i];
+        }
+        if(item[i] < yMin && i==1){
+            yMin = item[i];
+        }
+    }
+});
+let xAns = 0;
+let yAns = 0;
+if(xMax<0 && xMin<0){
+    xAns =(-1*xMin) - (-1*xMax);
+}else{
+    xAns = (xMax-xMin);
 }
-console.log((Math.max(...x) - Math.min(...x)) * (Math.max(...y) - Math.min(...y)));
+if(yMax<0 && yMin<0){
+    yAns =(-1*yMin) - (-1*yMax);
+}else{
+    yAns = (yMax-yMin);
+}
+
+console.log(xAns*yAns);

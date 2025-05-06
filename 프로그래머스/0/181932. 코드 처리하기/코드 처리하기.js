27a3ca8cd608cfc435d50682bf4edfef9964f911
DogGang <rlaehgusqp@naver.com>
2025-05-06 15:34:41 +0900

    [level 0] Title: 코드 처리하기, Time: 9.25 ms, Memory: 39.9 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -4,25 +4,12 @@ function solution(code) {
     var ret = "";
     answer = code.split("");
     for(let i=0; i<answer.length; i++){
-        if(mode===0){
-            if(answer[i] !=="1"){ //문자가 1인지 확인하고 바꾸는 과정 (1이 아니므로 안바꿈.)
-                if(i%2==0){
-                    ret+=answer[i];
-                }
-            }else if(answer[i] =="1"){
-                mode=1;
-            }
-        }else if(mode===1){
-            if(answer[i] !=="1"){ //문자가 1인지 확인하고 바꾸는 과정 (1이 아니므로 안바꿈.)
-                if(i%2==1){
-                    ret+=answer[i];
-                }
-            }else if(answer[i] =="1"){
-                mode=0;
-            }
+        if(answer[i] == "1"){
+            mode = mode===1 ? 0 : 1;
+        }else{
+            i%2==mode ? ret+=answer[i] : null;
         }
     }
-    
     if(ret==""){
         return "EMPTY";
     }else{

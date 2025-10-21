aff33256f8beb85fb9af882c07968de54de79337
DogGang <rlaehgusqp@naver.com>
2025-10-21 16:28:58 +0900

    [level 2] Title: 재구매가 일어난 상품과 회원 리스트 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
+++ 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -1,6 +1,5 @@
 -- 코드를 입력하세요
-SELECT user_id, product_id from online_sale
+select user_id, product_id from online_sale
 group by user_id, product_id
-having count(*) > 1
-order by user_id, product_id desc;
-
+having count(*)>1
+order by 1 asc, 2 desc;
